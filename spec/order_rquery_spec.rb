f4972342d6661c1490cfe1eee46ce8c448cc1db1
Andrew Kennedy and Kris Windham <team+andrew+kris@jackrussellsoftware.com>
2012-03-09 15:55:57 -0500

    added additional order by tests

--- spec/order_rquery_spec.rb
+++ spec/order_rquery_spec.rb
@@ -16,11 +16,19 @@ describe 'GET /resources' do
   end
 
   it 'should order by name ascending' do
-    get '/resources', :order => "[\"name\"]"
+    get '/resources', :order => "[\"name DESC\"]"
     last_response.headers["Content-Type"].should == 'application/json;charset=utf-8'
     last_response.should be_ok #checks status code 200
-    JSON.parse(last_response.body)['results'].first["name"].should == "afoo"
-    JSON.parse(last_response.body)['results'].first["description"].should == "abar"
+    JSON.parse(last_response.body)['results'].first["name"].should == "foo"
+    JSON.parse(last_response.body)['results'].first["description"].should == "bar"
+  end
+
+  it 'should order by name ascending' do
+    get '/resources', :order => "[\"created_at DESC\"]"
+    last_response.headers["Content-Type"].should == 'application/json;charset=utf-8'
+    last_response.should be_ok #checks status code 200
+    JSON.parse(last_response.body)['results'].first["name"].should == "cfoo"
+    JSON.parse(last_response.body)['results'].first["description"].should == "cbar"
   end
 
 end
